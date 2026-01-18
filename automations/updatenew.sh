55b7687c054203dfebb29005e7de7720afba867a
Ubuntu <ubuntu@ip-172-31-21-77.us-east-2.compute.internal>
2026-01-18 23:09:21 +0000

    added k8s,jenkins and terraform

---
+++ automations/updatenew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the Instance ID and path to the .env file
+INSTANCE_ID="i-0057478da95f160b2"
+
+# Retrieve the public IP address of the specified EC2 instance
+ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+# Path to the .env file
+file_to_find="../.env"
+
+# Check the current FRONTEND_URL in the .env file
+current_url=$(sed -n "4p" $file_to_find)
+
+# Update the .env file if the IP address has changed
+if [[ "$current_url" != "REACT_APP_API_URL=\"http://${ipv4_address}:8081\"" ]]; then
+    if [ -f $file_to_find ]; then
+        sed -i -e "s|REACT_APP_API_URL.*|REACT_APP_API_URL=\"http://${ipv4_address}:8081\"|g" $file_to_find
+    else
+        echo "ERROR: File not found."
+    fi
+fi
--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getAllUsers = () => axios.get(`${API_URL}/users`);
 export const createUser = (user) => axios.post(`${API_URL}/users`, user);
